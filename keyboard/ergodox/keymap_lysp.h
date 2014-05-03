00ea1daca952cd854f850f6ff27e6a976b253673
Yu-Xi Lim <limyuxi@gmail.com>
2014-05-03 19:53:01 +0800

    Mac-friendly keys and better mouse keys

--- keyboard/ergodox/keymap_lysp.h
+++ keyboard/ergodox/keymap_lysp.h
@@ -4,15 +4,15 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 0: Default Layer
      *
      * ,--------------------------------------------------.           ,--------------------------------------------------.
-     * |   ~    |   1  |   2  |   3  |   4  |   5  |  Esc |           |  Fn0 |   6  |   7  |   8  |   9  |   0  |   -    |
+     * |   ~    |   1  |   2  |   3  |   4  |   5  |  Esc |           |  Fn1 |   6  |   7  |   8  |   9  |   0  |   -    |
      * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
      * | Tab    |   Q  |   W  |   E  |   R  |   T  |  Ins |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   [    |
      * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
      * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
-     * |--------+------+------+------+------+------|  Fn2 |           |  Fn1 |------+------+------+------+------+--------|
+     * |--------+------+------+------+------+------| LGui |           | RGui |------+------+------+------+------+--------|
      * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
      * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-     *   | LGui | `    |   /  |  Lft | Rght |                                       |  Lft |  Up  |  Dn  | Rght | RGui |
+     *   |  Fn2 | `    |   /  |  Lft | Rght |                                       |  Lft |  Up  |  Dn  | Rght |  Fn0 |
      *   `----------------------------------'                                       `----------------------------------'
      *                                        ,-------------.       ,-------------.
      *                                        | LCtl | LAlt |       | RAlt | RCtl |
@@ -28,17 +28,17 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         EQL, 1,   2,   3,   4,   5,   ESC,
         TAB, Q,   W,   E,   R,   T,   INS,
         CLCK,A,   S,   D,   F,   G,
-        LSFT,Z,   X,   C,   V,   B,   FN2,
-        LGUI,GRV, BSLS,LEFT,RGHT,
+        LSFT,Z,   X,   C,   V,   B,   LGUI,
+        FN2, GRV, BSLS,LEFT,RGHT,
                                       LCTL,LALT,
                                            HOME,
                                  BSPC,DEL, END,
         // right hand
-             FN0, 6,   7,   8,   9,   0,   MINS,
+             FN1, 6,   7,   8,   9,   0,   MINS,
              RBRC,Y,   U,   I,   O,   P,   LBRC,
                   H,   J,   K,   L,   SCLN,QUOT,
-             FN1, N,   M,   COMM,DOT, SLSH,RSFT,
-                       LEFT,DOWN,UP,  RGHT,RGUI,
+             RGUI,N,   M,   COMM,DOT, SLSH,RSFT,
+                       LEFT,DOWN,UP,  RGHT,FN0,
         RALT,RCTL,
         PGUP,
         PGDN,ENT, SPC
@@ -49,8 +49,8 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,F1,  F2,  F3,  F4,  F5,  F6,
         TRNS,F7,  F8,  F9,  F10, F11, F12,
         TRNS,MPRV,MPLY,MNXT,VOLD,VOLU,
-        LSFT,TRNS,MSTP,TRNS,MUTE,MUTE,TRNS,
-        LGUI,PSCR,SLCK,PAUS,TRNS,
+        LSFT,MSTP,MSTP,NO,  MUTE,MUTE,LGUI,
+        TRNS,PSCR,SLCK,PAUS,NO,
                                       LCTL,LALT,
                                            TRNS,
                                  TRNS,TRNS,TRNS,
@@ -58,8 +58,8 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
              FN3, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
                   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,RSFT,
-                       TRNS,TRNS,TRNS,TRNS,RGUI,
+             RGUI,TRNS,TRNS,TRNS,TRNS,TRNS,RSFT,
+                       TRNS,TRNS,TRNS,TRNS,TRNS,
         RALT,RCTL,
         TRNS,
         TRNS,TRNS,TRNS
@@ -67,23 +67,23 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KEYMAP(  // layout: layer 2: mouse + numpad
         // left hand
-        TRNS,NO,  NO,  NO,  NO,  PAUS,PSCR,
-        TRNS,WH_L,WH_U,WH_D,WH_R,BTN2,TRNS,
-        TRNS,MS_L,MS_U,MS_D,MS_R,BTN1,
-        TRNS,NO,  NO,  NO,  NO,  BTN3,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
+		TRNS,NO,  BTN3,NO,  NO,  NO,  NO,
+        TRNS,BTN1,MS_U,BTN2,WH_U,WH_L,NO,
+        TRNS,MS_L,MS_D,MS_R,WH_D,WH_R,
+        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+        FN0, TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
              TRNS,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
-             SLCK,NO,  P7,  P8,  P9,  PMNS,BSPC,
+             TRNS,NO,  P7,  P8,  P9,  PMNS,BSPC,
                   NO,  P4,  P5,  P6,  PPLS,PENT,
              TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
-                       P0,  PDOT,SLSH,PENT,PENT,
+                       P0,  PDOT,SLSH,PENT,TRNS,
         TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
+        BTN1,
+        BTN1,TRNS,TRNS
     ),
 
     KEYMAP(  // layout: layer 3: Colemak layout
@@ -108,11 +108,6 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-/* id for user defined functions */
-enum function_id {
-    TEENSY_KEY,
-};
-
 /*
  * Fn action definition
  */
@@ -122,18 +117,3 @@ static const uint16_t PROGMEM fn_actions[] = {
     ACTION_DEFAULT_LAYER_SET(2),                    // FN2 - switch to Layer2
     ACTION_DEFAULT_LAYER_SET(3),                    // FN3 - switch to Layer3
 };
-
-void action_function(keyrecord_t *event, uint8_t id, uint8_t opt)
-{
-    print("action_function called\n");
-    print("id  = "); phex(id); print("\n");
-    print("opt = "); phex(opt); print("\n");
-    if (id == TEENSY_KEY) {
-        clear_keyboard();
-        print("\n\nJump to bootloader... ");
-        _delay_ms(250);
-        bootloader_jump(); // should not return
-        print("not supported.\n");
-    }
-}
-
